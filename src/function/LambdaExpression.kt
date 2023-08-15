3ad1725d79b666fe99f4d2b2be06a32604fdc3e1
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-15 16:54:57 +0700

    learn about function: add LambdaExpression.kt practice

---
+++ src/function/LambdaExpression.kt
@@ -0,0 +1,32 @@
+package function
+
+/**
+ * lambda expression : function yang tidak memiliki nama
+ * langsung include pada variable, array dll
+ */
+
+fun main() {
+    val requestCode: (Int, String) -> String = {resultHttp: Int, message: String ->
+        val result = "{result: $resultHttp, message: $message}"
+        result
+    }
+
+    val result = requestCode(200,"Success")
+    println(result)
+
+    //hanya jalan jika 1 parameter
+    val name: (String) -> String = {
+        "hello $it"
+    }
+
+    val names = name("Ichwan")
+    println(names)
+
+    //method references
+    fun login (username: String, password: String): String{
+        return "Username: $username, Password: $password"
+    }
+
+    val loginUser : (String, String) -> String = ::login
+    println(loginUser("Ichwan","123abc"))
+}
