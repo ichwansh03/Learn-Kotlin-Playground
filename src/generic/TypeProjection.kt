5fcc955e85bc3717d7e6df37a23bae4f3d8aaf59
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-17 14:40:23 +0700

    learn about generic: add star projection in TypeProjection.kt

--- src/generic/TypeProjection.kt
+++ src/generic/TypeProjection.kt
@@ -2,7 +2,13 @@ package generic
 
 /**
  * menambahkan info covariant dan contravariant di @param function
+ * tanda asterix (*) berarti tipe yang digunakan bebas
  */
+
+fun DataLength(array: Array<*>){
+    println("length of array is ${array.size}")
+}
+
 class Sentences<T>(var tense: T)
 
 fun copy(from: Sentences<out Any>, to: Sentences<in Any>){
@@ -14,4 +20,7 @@ fun main() {
     val sentences2: Sentences<Any> = Sentences("Data 2")
 
     copy(sentences, sentences2)
+
+    val arraySize = arrayOf(1, 2, "3", 4, 5.0, "A")
+    DataLength(arraySize)
 }
